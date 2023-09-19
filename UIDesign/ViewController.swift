fcd56ac6f3111f8d957b484fff09fa534d1d7c8b
Pratik Morge <pratikmorge62@gmail.com>
2023-09-19 14:29:20 +0530

    Initial Commit

---
+++ UIDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIDesign
+//
+//  Created by Pratiksha Jagtap on 19/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
